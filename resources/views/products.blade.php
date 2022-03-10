aa07e0dd78b4957db94bfcd617bb3c58093596b0
ncla <ncla@users.noreply.github.com>
2022-03-10 19:19:44 +0200

    Form; product PUT test endpoint

---
+++ resources/views/products.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MIG</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="">
+        <form method="POST" action="{{ route('product.create') }}">
+            @csrf
+            @method('PUT')
+
+            <label for="ean_13">EAN-13:</label>
+            <input type="text" id="ean_13" name="ean_13" maxlength="13">
+
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+
+            <label for="quantity">Quantity:</label>
+            <input type="number" id="quantity" name="quantity" required>
+
+            <label for="initial_cost">Initial cost:</label>
+            <input type="number" id="initial_cost" name="initial_cost" required>
+
+            <label for="price_with_tax">Price with tax:</label>
+            <input type="number" id="price_with_tax" name="price_with_tax" required>
+
+            <button type="submit">Save</button>
+        </form>
+    </body>
+</html>
